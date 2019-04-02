6a72094ad8eb6b0fc97429da4dd6f7bc9863025c
PWoltman <paul.woltman@imsbarter.com>
2019-04-02 13:29:39 -0500

    Update engine.rb

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -17,6 +17,20 @@ require 'plupload-rails'
 
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
+    
+    initializer "comfortable_mexican_sofa.setup_assets" do
+      ::ComfortableMexicanSofa::Engine.config.assets.precompile += %w[
+        comfy/admin/cms/application.js
+        comfy/admin/cms/application.css
+        comfy/admin/cms/lib/glyphicons-halflings-regular.svg
+        comfy/admin/cms/lib/glyphicons-halflings-regular.ttf
+        comfy/admin/cms/lib/glyphicons-halflings-regular.woff
+        comfy/admin/cms/lib/glyphicons-halflings-regular.woff2
+        comfy/admin/cms/lib/redactor-font.eot
+      
+      ]
+    end
+    
     config.to_prepare do
       Dir.glob(Rails.root + "app/decorators/comfortable_mexican_sofa/*_decorator*.rb").each do |c|
         require_dependency(c)
